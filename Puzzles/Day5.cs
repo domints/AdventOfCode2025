fa29769555d1df93f0063de8440f7e2724826c5f
Dominik Szymański <domints@gmail.com>
2025-12-11 19:17:58 +0100

    Add solution for day 5

--- Puzzles/Day5.cs
+++ Puzzles/Day5.cs
@@ -18,7 +18,7 @@ namespace AdventOfCode2025.Puzzles
             {
                 foreach (var range in ranges)
                 {
-                    if (id >= range.start && id <= range.end)
+                    if (id >= range.Start && id <= range.End)
                     {
                         count++;
                         break;
@@ -33,19 +33,45 @@ namespace AdventOfCode2025.Puzzles
         {
             var inputEnumerator = input.GetEnumerator();
             var ranges = LoadRanges(inputEnumerator);
-            var set = new HashSet<long>();
+            var count = 0L;
+            var counted = new List<Range>();
             foreach (var range in ranges)
             {
-                for (long i = range.start; i <= range.end; i++)
-                    set.Add(i);
+                List<Range> leftRanges = [range];
+                foreach (var ct in counted)
+                {
+                    List<Range> step = new();
+                    bool changed = false;
+                    foreach (var left in leftRanges)
+                    {
+                        if (!Overlap(left, ct))
+                        {
+                            step.Add(left);
+                            continue;
+                        }
+
+                        changed = true;
+
+                        step.AddRange(p1_excluding_p2(left, ct));
+                    }
+
+                    if (changed)
+                        leftRanges = step;
+                }
+
+                foreach (var r in leftRanges)
+                {
+                    count += r.End - r.Start + 1;
+                    counted.Add(r);
+                }
             }
 
-            return set.Count;
+            return count;
         }
 
-        private List<(long start, long end)> LoadRanges(IEnumerator<string> input)
+        private List<Range> LoadRanges(IEnumerator<string> input)
         {
-            List<(long start, long end)> ranges = [];
+            List<Range> ranges = [];
             while (input.MoveNext() && !string.IsNullOrWhiteSpace(input.Current))
             {
                 var line = input.Current;
@@ -66,5 +92,30 @@ namespace AdventOfCode2025.Puzzles
 
             return ids;
         }
+
+        private bool Overlap(Range x, Range y)
+        {
+            return x.Start <= y.End && y.Start <= x.End;
+        }
+
+        private List<Range> p1_excluding_p2(Range p1, Range p2)
+        {
+            if (p1.End < p2.Start) return [p1]; // line p1 finishes before the exclusion line p2
+            if (p1.Start > p2.End) return [p1]; // line p1 starts after exclusion line p1
+            List<Range> lines = [];
+            // calculate p1 before p2 starts
+            var line1 = new Range(p1.Start, Math.Min(p1.End, p2.Start - 1));
+            if (line1.Start <= line1.End) lines.Add(line1);
+            // calculate p1 after p2 ends
+            var line2 = new Range(p2.End + 1, p1.End);
+            if (line2.Start <= line2.End) lines.Add(line2);
+            // these contain the lines we calculated above
+            return lines;
+        }
+
+        record Range(long Start, long End)
+        {
+            public static implicit operator Range((long start, long end) v) => new Range(v.start, v.end);
+        };
     }
 }
